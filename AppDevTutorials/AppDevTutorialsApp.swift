deb9c6f082bb50fbc86e2a8befd8e2d7bc37fe94
Mykhailo Tokmakov <mishkabalooo@gmail.com>
2025-10-13 15:25:29 +0300

    Initial Commit

---
+++ AppDevTutorials/AppDevTutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppDevTutorialsApp.swift
+//  AppDevTutorials
+//
+//  Created by Michael on 10/13/25.
+//
+
+import SwiftUI
+
+@main
+struct AppDevTutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
